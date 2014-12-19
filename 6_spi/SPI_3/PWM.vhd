8bc77870922b36dd196e94f158d50e0f8929eb10
Kyle Finch <finch.kyle14@gmail.com>
2014-12-18 21:49:16 -0800

    Spi stuff

---
+++ 6_spi/SPI_3/PWM.vhd
@@ -0,0 +1,71 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    20:15:49 12/11/2014 
+-- Design Name: 
+-- Module Name:    PWM - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity PWM is Port(
+	 CLK 		: in std_logic;
+	 DATA		: in std_logic_vector(7 downto 0);
+      START    : in std_logic; 
+      pwmOut     : out std_logic;
+	 DONE 		: out std_logic);
+end PWM;
+
+architecture Behavioral of PWM is
+
+signal count : std_logic_vector(7 downto 0);
+
+begin
+	process (CLK, START) begin
+		if(clk'event and clk='1') then
+			if (START = '1') then	
+				if (count = 255) then 
+					DONE <= '1';
+					count <= x"00";
+				else 
+					DONE<= '0';
+					count <= count + 1;
+				end if;	
+			end if;
+		end if;
+	end process;
+	
+	process(count) begin
+		if(count <= DATA) then
+			pwmout <= '1';
+		else
+			pwmout <='0';
+		end if;
+	end process;
+	
+	
+end Behavioral;
+
---
+++ 6_spi/SPI_3/topSPI.vhd
@@ -0,0 +1,52 @@
+--Kyle Finch
+--11193077
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity topSPI is
+    Port ( CLK : in  STD_LOGIC;
+           RST : in  STD_LOGIC;
+           pwmOUT : out  STD_LOGIC;
+           SCLK : out  STD_LOGIC;
+           CS : out STD_LOGIC;
+           SDATA : in  STD_LOGIC;
+		 bt1 : in STD_LOGIC;
+		 sw : in STD_LOGIC_VECTOR(7 downto 0));
+end topSPI;
+
+architecture Behavioral of topSPI is
+
+component MIC is Port(    
+    CLK      : in std_logic;         
+    RST      : in std_logic;
+    SDATA   : in std_logic;
+    SCLK     : out std_logic;
+    nCS      : out std_logic;
+    DATA    : out std_logic_vector(7 downto 0);
+    START    : in std_logic; 
+    DONE     : out std_logic);
+end component MIC;
+
+component PWM is Port(
+	CLK 		: in std_logic;
+	DATA		: in std_logic_vector(7 downto 0);
+     START    : in std_logic; 
+     pwmOut     : out std_logic;
+	DONE 		: out std_logic);
+end component PWM;
+
+signal DATA :std_logic_vector(7 downto 0);
+signal pDONE :std_logic;
+signal DONE :std_logic;
+
+begin
+
+C0: component MIC port map (CLK=>CLK, RST=>RST, SDATA=>SDATA, SCLK=>SCLK, nCS=>CS, DATA=>DATA, START=>'1', DONE=>DONE);
+C1: component PWM port map(CLK=>CLK, DATA=>DATA, START=>'1', pwmOut=>pwmOut, DONE=>pDONE);
+
+
+end Behavioral;
+
